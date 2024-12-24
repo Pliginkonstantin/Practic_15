dc954fadc8e9a19ec2bb3e6ba98e127b3bdc28c3
Pliginkonstantin <falsekiller3@gmail.com>
2024-12-24 09:25:57 +0200

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 4. Генерируются 10 случайных целых чисел в интервале (-40, 40). Вывести эти числа.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            Random Rand = new Random();
+            for (int x = 1; x <= 10; x++)
+            {
+                int num = Rand.Next(-40, 40);
+                Console.WriteLine($"{x} : {num}");
+            }
+        }
+    }
+}
