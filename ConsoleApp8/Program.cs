543cf1d91c7d237499a8c94905d454eb10455b0a
Pliginkonstantin <falsekiller3@gmail.com>
2024-12-24 09:01:49 +0200

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 1. Напечатать ряд чисел 20 в виде: 20 20 20 20 20 20 20 20 20 20.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func()
+        {
+            for(int x = 0; x <= 10; x++)
+            {
+                Console.Write($"{20}\t");
+            }
+        }
+    }
+}
